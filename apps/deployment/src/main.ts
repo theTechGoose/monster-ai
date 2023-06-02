7f5388e83a414c9c2d4d0b4e9a7500b88792798a
Raphael Castro <raphael@rcincorporated.net>
2023-06-02 14:58:56 -0400

    file watcher

--- apps/deployment/src/main.ts
+++ apps/deployment/src/main.ts
@@ -7,12 +7,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import chokidar from 'chokidar'
+import os from "os"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
@@ -20,3 +22,5 @@ async function bootstrap() {
 }
 
 bootstrap();
+const watcher = chokidar.watch(`${os.homedir()}/recordings`)
+watcher.on('add', path => console.log(path))
