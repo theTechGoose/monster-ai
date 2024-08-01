8dd75cfb4b4d589b0298065a2a0ef7216f900787
Raphael <raphael@rcincorporated.net>
2024-08-01 18:10:41 -0400

    catchup

--- apps/five-nine-recordings/src/main.ts
+++ apps/five-nine-recordings/src/main.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import express from 'express'
+import proxy from 'express-http-proxy'
 import { startCallIdentification } from './v2/call-identification-queue/call-identification';
 import { startCallTranscription } from './v2/transcription/transcription';
 import { startCallSummary } from './v2/summary/summary';
@@ -9,7 +11,37 @@ export const IDENTIFY_RECORDING_UPDATE_INTERVAL = 500;
 export const UPDATE_INTERVAL = 1000;
 export const MAX_RETRIES = 3;
 
+
+
+
+  
+const app = express()
+const router = express.Router();
+
+
+router.use('/llm/generate',  (req, res) => {
+  const headers = req.headers
+  if(headers['x-pass'] !== 'we are the ai Monsters!1') {
+    res.status(401).send({ message: 'unauthorized'})
+    return
+  }
+  
+  
+  return proxy('localhost:8000',{
+  proxyReqPathResolver: function () {
+      return '/api/generate'
+    }})(req, res)
+})
+
+app.use(router)
+
+app.listen(3000,() => {
+  
 startCallIdentification();
 startCallTranscription();
 startCallSummary();
 startSendToCrm();
+  
+  
+})
+  
