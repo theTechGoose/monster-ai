7207a80514984ad75ac60ff0722cf5fa6888bebf
Raphael <raphael@rcincorporated.net>
2024-08-17 22:00:06 -0400

    catchup

--- apps/five-nine-recordings/src/main.ts
+++ apps/five-nine-recordings/src/main.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express'
 import proxy from 'express-http-proxy'
+import {countTokens, getModel} from './local-llm/index'
 import { startCallIdentification } from './v2/call-identification-queue/call-identification';
 import { startCallTranscription } from './v2/transcription/transcription';
 import { startCallSummary } from './v2/summary/summary';
 import { startSendToCrm } from './v2/send-to-crm/send-to-crm';
+import { jobManager } from './v2/queue';
 
 export const ENV = 'prod';
 export const IDENTIFY_RECORDING_UPDATE_INTERVAL = 500;
@@ -29,19 +31,20 @@ router.use('/llm/generate',  (req, res) => {
   
   return proxy('localhost:8000',{
   proxyReqPathResolver: function () {
-      return '/api/generate'
+      return '/api/chat'
     }})(req, res)
 })
 
 app.use(router)
 
-app.listen(3000,() => {
-  
+app.listen(3000,async () => {
 startCallIdentification();
 startCallTranscription();
 startCallSummary();
 startSendToCrm();
-  
-  
 })
-  
+
+
+
+
+
